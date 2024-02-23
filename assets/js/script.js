356e296492373dbaff10f35ed91ce315ec595ef4
Daniela Álvarez <danielapaz.aru@gmail.com>
2024-02-23 17:47:30 -0300

    se agrega Jquery, función toggle para la sección ¿quienes somos? y alerta de mensaje al apretar el botón de enviar en el formulario

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $("#enviarCorreo").click(function () {
+        alert("El correo fue enviado correctamente");
+    });
+    $("#fondoIcon1").click(function () {
+        $(".parrafo1").toggle();
+    });
+    $("#fondoIcon2").click(function () {
+        $(".parrafo2").toggle();
+    });
+    $("#fondoIcon3").click(function () {
+        $(".parrafo3").toggle();
+    });
+});
